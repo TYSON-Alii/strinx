a6887cd090a4badfe69f30caa644fcebaba45def
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-10-06 20:31:16 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "strinx.hpp"
+
+using namespace std;
+
+int main() {
+    strinx yazi = "cok duzgun bir yazi";
+    cout << yazi.random() << '\n'; // artik degil :c
+    return 0;
+}
